a8f9197d5d89d6b2ba29859483c2aac5483e8d72
Krishna Patel <kriishnapat@gmail.com>
2022-03-01 10:09:21 -0700

    cvbvcb

--- Program.cs
+++ Program.cs
@@ -4,9 +4,25 @@ namespace sortFunction
 {
     class Program
     {
+        static int insertionSort(int[] anArray){
+            for (int i=1; i<anArray.Length-1; i++){
+                int insert_value = anArray[i];
+                int position = i-1;
+                while(position >= 0 && anArray[position] > insert_value){
+                    anArray[position+1] = anArray[position];
+                    position = position - 1;
+                }
+                anArray[position+1] = insert_value;
+            }
+            foreach (int x in anArray)
+                Console.Write(x + "  ");
+            return 0;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] nums = {10, 70, 30, 100, 40, 45, 90, 80, 85};
+            string[] words = {"dog","at", "good", "eye", "cat", "ball", "fish"};
+            Console.WriteLine(insertionSort(nums));
         }
     }
 }
